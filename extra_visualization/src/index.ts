48f15c040d531ecdf4c668ffec90943c6c5e7148
unknown <luca.munekata@gmail.com>
2026-06-26 11:10:46 -0400

    fixing pwv2

--- extra_visualization/src/index.ts
+++ extra_visualization/src/index.ts
@@ -1,7 +1,8 @@
 /*
  * index.ts
  */
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: path.join(path.dirname(import.meta.filename), '../../.env') });
 var config:any = readFileAsJSON("./config.json")
 
 import * as fs from 'fs';
